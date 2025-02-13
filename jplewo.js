38094f9b24ac46f42f04a3e4f9266f4157a03730
imexcelp <excel_imposts_0h@icloud.com>
2025-02-13 08:54:45 -0600

    Create jplewo.js

---
+++ jplewo.js
@@ -0,0 +1,24 @@
+// to Hide Logo
+(() => {
+  function hideLogo() {
+    const hideLogoStyles = `
+      div:has(> div > img[src="/logo.png"]) {
+        display: none !important;
+      }
+    `;
+
+    const styleElement = document.createElement('style');
+    styleElement.textContent = hideLogoStyles;
+    document.head.appendChild(styleElement);
+
+    console.log('logo hiding ext loaded successfully.');
+  }
+
+  // Check if the document is already loaded
+  if (document.readyState === 'complete' || document.readyState === 'interactive') {
+    hideLogo();
+  } else {
+    // If not, wait for it to load
+    document.addEventListener('DOMContentLoaded', hideLogo);
+  }
+})();
